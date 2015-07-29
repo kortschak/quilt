33257ca871411af1640b641234842b80854b6611
kortschak <dan.kortschak@adelaide.edu.au>
2015-07-29 12:34:26 +0930

    improve cost function

--- stitch.go
+++ stitch.go
@@ -86,35 +86,44 @@ func main() {
 			return math.Inf(-1), false
 		}
 
-		score = left.score
-
-		var overlap int
-
-		// Genomic coordinate term.
-		overlap = left.genomic.right - right.genomic.left
-		if overlap > 0 {
-			// Case where there is a back step.
-			score -= float64(overlap) * 10
-		} else {
-			// Case where there is a separation.
-			score -= float64(overlap) * -1
-		}
-
-		// Repeat coordinate term.
+		gOverlap := left.genomic.right - right.genomic.left
+		var rOverlap int
 		if right.genomic.strand == seq.Plus {
-			overlap = left.right - right.left
+			rOverlap = left.right - right.left
 		} else {
-			overlap = right.right - left.left
-		}
-		if overlap > 0 {
-			// Case where there is a back step.
-			score -= float64(overlap) * 500
-		} else {
-			// Case where there is a separation.
-			score -= float64(overlap) * -10
+			rOverlap = right.right - left.left
 		}
 
-		return score, true
+		cost := float64(gOverlap) * float64(rOverlap) * float64(gOverlap-rOverlap)
+		switch {
+		// Special-case immediately adjacent intervals.
+		case rOverlap == 0:
+			if gOverlap < 0 {
+				cost = float64(gOverlap) * 2
+			} else {
+				cost = float64(gOverlap) * 100
+			}
+		case gOverlap == 0:
+			if rOverlap < 0 {
+				cost = float64(rOverlap) * 40
+			} else {
+				cost = float64(rOverlap) * 100
+			}
+		case rOverlap < 0 && gOverlap < 0:
+			// Separated parts.
+			cost *= 10
+		case rOverlap < 0 && gOverlap > 0:
+			// Overlapping genomic segments from different non-overlapping element parts.
+			cost *= 100
+		case rOverlap > 0 && gOverlap < 0:
+			// Overlapping element parts from different non-overlapping genome segments.
+			cost *= 100
+		default:
+			// Co-overlaps.
+			cost *= 10
+		}
+
+		return left.score - math.Abs(cost), true
 	}
 
 	const maximumSeparation = 5e4
